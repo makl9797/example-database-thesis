e007d48992c4c27a1794547c169fe28f78ac51f0
Mats <mats@sourceboat.com>
2022-03-04 09:14:51 +0100

    add init_history_db script

--- pgMemento/INIT_HISTORY_DB.sql
+++ pgMemento/INIT_HISTORY_DB.sql
@@ -1,14 +1,6 @@
-\echo 'Install pg_memento...'
-\i pgMemento/INSTALL_PGMEMENTO.sql
-
-\echo 'Initialize pg_memento...'
-
 SELECT pgmemento.init(
   log_old_data := TRUE,                      -- default is true
   log_new_data := TRUE,                      -- default is false
   log_state := TRUE,                         -- default is false
   trigger_create_table := TRUE               -- default is false
 );
-
-\echo 'Create db without history'
-CREATE DATABASE no_history_db;
